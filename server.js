363ed23aae689c1ba5799c16e68ab7a830961795
Paul Wentzel <paul.wentzel@gmail.com>
2024-07-10 10:35:11 -0400

    practice complete

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 // Your code here
+const http = require('http');
+
+
+const responseBody = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World!</title>
+</head>
+<body>
+  <h1>Hello there!</h1>
+</body>
+</html>
+`
+
+
+const server = http.createServer((req, res) => {
+  console.log('Request for', req.url);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(responseBody);
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('Server is listtening on port', port));
